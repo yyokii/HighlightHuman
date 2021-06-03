7ccbd6e47fff213fcada237c1fa18beb3cebe086
yyokii <yyokii.h@gmail.com>
2021-06-04 03:41:50 +0900

    Initial Commit

---
+++ HighlightHuman/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HighlightHuman
+//
+//  Created by Higashihara Yoki on 2021/06/04.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
